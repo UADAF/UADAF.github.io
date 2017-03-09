997925e56953687e5ec16921b2cf0c169bc37d4c
gt22 <igorengel@mail.ru>
2017-03-09 20:06:53 +0300

    Discord invite now extracting from widget.json

--- dev/ts/components/Misc/DiscordConnect.tsx
+++ dev/ts/components/Misc/DiscordConnect.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
-
+import {ajax} from "jquery";
+const invite = ajax({
+	url: 'https://discordapp.com/api/guilds/197699632841752576/widget.json',
+	async: false,
+}).responseJSON['instant_invite'];
 export default class DiscordConnect extends React.Component<{}, {}> {
 
 	render() {
 		return (
 			<div className="discord_connect">
 				<a className="connect_link"
-				   href="https://discord.gg/Mmzakpx"
+				   href={invite}
 				   target="_blank">
 					<div>
 						<img className="discord_logo img-responsive" src="images/discord/discordface.png"
