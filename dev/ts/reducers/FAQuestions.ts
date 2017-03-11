4c8f80dd1bd6bf1b94968239dc90de5d85e34a76
gt22 <igorengel@mail.ru>
2017-03-12 00:28:56 +0300

    Reducer optimization

--- dev/ts/reducers/FAQuestions.ts
+++ dev/ts/reducers/FAQuestions.ts
@@ -1,11 +1,5 @@
 import {HelpQuestionProps} from "../components/Misc/HelpQuestion";
 import {ajax} from "jquery";
 export default function (state = null): HelpQuestionProps[] {
-    if(state) {
-        return state;
-    }
-    return ajax({
-        url: "json/faq.json",
-        async: false
-    }).responseJSON;
+	return state || ajax({url: "json/faq.json", async: false}).responseJSON;
 }
--- dev/ts/reducers/VkLinks.ts
+++ dev/ts/reducers/VkLinks.ts
@@ -1,11 +1,5 @@
 import {VkContactProps} from "../components/Vk/VkContact";
 import {ajax} from "jquery";
 export default function (state = null): VkContactProps[] {
-	if(state) {
-		return state;
-	}
-	return ajax({
-		url: "json/vk.json",
-		async: false
-	}).responseJSON;
+	return state || ajax({url: "json/vk.json", async: false}).responseJSON;
 }
