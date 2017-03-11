1be7f146e5b575144bfb9c26aa2391a70bcfd34a
gt22 <igorengel@mail.ru>
2017-03-11 23:30:24 +0300

    Little changes

--- dev/ts/components/Misc/HelpQuestion.tsx
+++ dev/ts/components/Misc/HelpQuestion.tsx
@@ -8,7 +8,7 @@ export default class HelpQuestion extends React.Component<HelpQuestionProps, {}>
     render() {
         return (
             <div className="helpQuestion">
-                <p className="question"><span className="glyphicon glyphicon-asterisk"></span> {this.props.question}?</p>
+                <p className="question"><span className="glyphicon glyphicon-asterisk"/> {this.props.question}?</p>
                 <p className="answer">{this.props.answer}</p>
                 {this.props.images.map((e, i) => <img src={e} key={i} />)}
             </div>
